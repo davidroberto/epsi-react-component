67418f7b3742ae758778d2f2ea805da944c3de89
David Robert <contact@david-robert.fr>
2024-10-23 13:47:57 +0200

    create component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import Header from "./Header";
+
+// on créé un composant : une fonction dont le but est de créer un bout de page en HTML
+// grâce au langage JSX
 function App() {
+  // on est dans le coeur de la fonction / du composant
+  // avant le return, on peut utiliser tout le js qu'on veut
+
+  // avant le return, le but est de préparer des données dans des variables
   const articles = [
     { id: 1, title: "React" },
     { id: 2, title: "Angular" },
     { id: 3, title: "Vue" },
   ];
 
+  const title = "Titre de la page";
+
+  // le return doit retourner du JSX
+  // le language que React a créé, qui permet de
+  // créer des éléments HTML en JS (comme document.createElement,
+  // mais avec une syntaxe plus facile / jolie / proche du HTML
+  // tout en gardant certaines capacités du JS : boucles, conditions, etc.
+
   return (
     <div>
-      <h1>Articles</h1>
+      <Header />
+
       <ul>
         {articles.map((article) => (
           <li key={article.id}>{article.title}</li>
